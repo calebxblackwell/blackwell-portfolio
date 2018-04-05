5f3ee9b36a4c6ac19c767da3ee6641a5e4d75446
Caleb Blackwell <calebxblackwell@gmail.com>
2018-04-05 12:57:37 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//typing effect
+var i = 0;
+var txt = 'Full Stack Programmer/Developer. I code, vlog, get too caffeinated and spend most of my time buying things from the gas station.';
+var speed = 50;
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("aboutme").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
