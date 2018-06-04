bf69a9c334b949bdb1650d6f0053c1302fadf0c9
Caleb Blackwell <calebxblackwell@gmail.com>
2018-06-04 14:11:42 -0700

    commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //typing effect
 var i = 0;
-var txt = 'Full Stack Programmer/Developer. I code, vlog, get too caffeinated and spend most of my time buying things from the gas station.';
+var txt = 'Full Stack Programmer/Developer. I code, get too caffeinated and spend most of my time buying things from the gas station.';
 var speed = 50;
 function typeWriter() {
   if (i < txt.length) {
@@ -8,33 +8,4 @@ function typeWriter() {
     i++;
     setTimeout(typeWriter, speed);
   }
-}
-//slideshow area
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
